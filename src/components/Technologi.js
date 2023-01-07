742e17a58bd2ac2a933389237b3d3b877675834c
Darshankhokhariya <darshankhokhariya26@gmail.com>
2023-01-07 10:20:48 +0530

    varni

--- src/components/Technologi.js
+++ src/components/Technologi.js
@@ -5,8 +5,6 @@ function Technologi() {
     return (
         <div className='h-20 w-full bg-white  flex border-b-2 items-center mt-5'>
                  <div className='flex m-auto items-center'>
-                    <Link></Link>
-                    <Link></Link>
                     <div className='mx-4 text-xl hover:text-[#0093ed]  hover:border-b-2 transition duration-100'>Frontend</div>
                     <div className='mx-4 text-xl hover:text-[#0093ed]  hover:border-b-2 transition duration-100'>Backend</div>
                 </div>
