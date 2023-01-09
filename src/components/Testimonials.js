ed7c8a17ec414e02fc913feec2919ba8075e349d
Darshankhokhariya <darshankhokhariya26@gmail.com>
2023-01-09 11:55:35 +0530

    varni

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -9,7 +9,7 @@ function Testimonials() {
                 <h1 className='mt-4 pt-5 mb-2 text-center capitalize text-4xl'>Our Customers Love Us. Here’s Why.</h1>
                 <hr className='w-1/5 mx-auto  border-2' />
                 <Carousel>
-                    <div className='grid grid-cols-2'>
+                    <div className='grid lg:grid-cols-2 sm:grid-cols-1'>
                         <div className='p-5 border'>
                             <h4>Andrei Popa</h4>
                             <p className="">Our second large project with Vasundhara Infotech. They have proven over time a dedication and passion to the project second to none. We are currently working and will carry on with them for the years to come.</p>
