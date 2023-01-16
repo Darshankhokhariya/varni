e22fefe0c92804ab9a5e9839912f99b81afc0012
Darshankhokhariya <darshankhokhariya26@gmail.com>
2023-01-16 11:29:41 +0530

    varni

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -70,15 +70,15 @@ function Footer() {
                     </div>
                     <div>
                         <form>
-                            <div class="mb-6">
+                            <div class="mb-10 mx-28">
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                 <input type="email" id="email"  onChange={(e) => {setemail(e.target.value)}}   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required/>
                             </div>
-                            <div class="mb-6">
+                            <div class="mb-6 mx-28">
                                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
                                 <input type="name" id="password"  onChange={(e) => {setname(e.target.value)}}   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Name' required/>
                             </div>
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={setval}>register</button>
+                            <button type="button" class="text-white bg-blue-700 mx-28 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={setval}>register</button>
                         </form>
                     </div>
                 </div>
